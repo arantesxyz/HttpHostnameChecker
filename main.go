125d848a7799e097378e9448e4e30fee74494528
Gustavo Arantes <me@arantes.dev>
2021-02-06 17:56:54 -0300

    Convert response to json

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+type info struct {
+	Message string `json:"message"`
+	Source  string `json:"source"`
+	Method  string `json:"method"`
+	Host    string `json:"host"`
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(info{
+		Message: "Hello World!!!",
+		Source:  "https://github.com/arantesxyz/HttpHostnameChecker",
+		Method:  r.Method,
+		Host:    r.Host,
+	})
+
 	fmt.Println("Received " + r.Method + " request from " + r.RemoteAddr + " with host " + r.Host)
-	fmt.Fprintf(w, "Source: https://github.com/arantesxyz/HttpHostnameChecker")
-	fmt.Fprintf(w, "\nMethod: "+r.Method)
-	fmt.Fprintf(w, "\nHost: "+r.Host)
 }
 
 func main() {
@@ -18,6 +31,6 @@ func main() {
 	http.HandleFunc("/", home)
 
 	port := os.Getenv("PORT")
-	fmt.Printf("Starting server on port %s...", port)
+	fmt.Printf("Starting server on port %s...\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
